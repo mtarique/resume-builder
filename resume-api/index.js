f69bff0ac005ed8bcca1945ebcd7c258b16d7796
mtarique <mtarique@outlook.com>
2024-09-19 01:09:55 +0530

    Updated to run all the containers

--- resume-api/index.js
+++ resume-api/index.js
@@ -1,8 +1,17 @@
 // Import Express
 const express = require('express');
+const mongoose = require('mongoose'); 
 
 // Initialize the app
 const app = express();
+
+require('dotenv').config(); 
+
+// Connect to mongodb database
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGODB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true}); 
+mongoose.connection.on("error", err => console.log("MongoDB could not establish a connection: ", err)); 
+
 
 // Define a port
 const port = process.env.PORT || 3000;
